e2d2c1e2b1051b00e7b126c6f7c3779f7f924ef6
niccioci <128808116+niccioci@users.noreply.github.com>
2023-07-26 13:09:09 +0100

    Update settings.py

--- investment_bot/settings.py
+++ investment_bot/settings.py
@@ -78,6 +78,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 
     'import_export',
     'mathfilters',
