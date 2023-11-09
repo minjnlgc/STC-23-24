c62b2943406936cc8decc24eb032323327bdb39c
minjnlgc <kunyuquemian@outlook.com>
2023-11-09 19:50:11 +0000

    Modified the questionnaire appearance

--- chatbot/static/chatbot/js/questionnaire.js
+++ chatbot/static/chatbot/js/questionnaire.js
@@ -1,65 +1,92 @@
 d3.select(window).on("load", function () {
-
     var log = {};
 
     var min_text_length = 10;
 
-    // console.log('data:', data);
+    console.log("data:", data);
     data.reverse();
-    data.forEach( (item, item_no) => {
+
+    data.forEach((item, item_no) => {
         //
         item_no = data.length - item_no - 1;
-        var row = d3.select('form')
-                    // .append('div')
-                    .insert("div",":first-child")
-                    // .insert("div",":last-child")
-                    .attr('class', 'form-row');
-        var col = row.append('div')
-                     .attr('class', 'col-12');
-        
-        if ('label' in item) {
-            col.append('span')
-               .html(item['label']);
-        } else if ('question' in item) {
-            var div = col.append('div')
-                         .attr('class' , 'form-group');
-            
-            div.append('label')
-            //    .attr('for', ``)
-                .html(item['question'])
+        var row = d3
+            .select("form")
+            // .append('div')
+            .insert("div", ":first-child")
+            // .insert("div",":last-child")
+            .attr("class", "form-row");
+        var col = row.append("div").attr("class", "col-12");
 
-            if ('choices' in item) {
+        if ("label" in item) {
+            col.append("span").html(item["label"]);
+        } else if ("question" in item) {
+            var div = col.append("div").attr("class", "form-group");
+
+            div
+                .append("label")
+                //    .attr('for', ``)
+                .html(item["question"]);
+
+            // wrap the choices
+            var choiceContainer = div.append("div").attr("class", "d-flex flex-column");
+            // choiceContainer.append('hr')
+            var choiceDiv = choiceContainer.append("div").attr("class", "d-flex justify-content-between");
+
+            if ("choices" in item) {
                 // multiple choice question
-                item['choices'].forEach( (choice, i) => {
-                    var html = `
-                    <input class="form-check-input item${item_no}-radios form-control" 
-                           type="radio" 
-                           name="item${item_no}-radios" 
-                           id="item${item_no}-radio${i}" 
-                           value="option${i}" 
-                           required>
-                       <label class="form-check-label" 
-                              for="item${item_no}-radio${i}">${choice}</label>`;
-                    var item_div = div.append('div')
-                       .attr('class', 'form-check')
-                       .html(html);
-                    if (i === (item.choices.length - 1)) {
-                        item_div.append('div')
-                                .attr('class', "invalid-feedback")
-                                .html('Please select an answer.');
+                item["choices"].forEach((choice, i) => {
+                    // var html = `
+                    // <input class="form-check-input item${item_no}-radios form-control"
+                    //        type="radio"
+                    //        name="item${item_no}-radios"
+                    //        id="item${item_no}-radio${i}"
+                    //        value="option${i}"
+                    //        required>
+                    //    <label class="form-check-label"
+                    //           for="item${item_no}-radio${i}">${choice}</label>`;
+
+                    var html = `<input class="form-check-input item${item_no}-radios form-control" 
+                                     type="radio" 
+                                     name="item${item_no}-radios" 
+                                     id="item${item_no}-radio${i}" 
+                                     value="option${i}" 
+                                     required>
+                                 <label class="form-check-label" 
+                                        for="item${item_no}-radio${i}">${choice}</label>`;
+
+                    // var item_div = div.append('div')
+                    //    .attr('class', 'form-check')
+                    //    .html(html);
+
+                    var item_div = choiceDiv
+                        .append("div")
+                        .attr("class", "form-check form-check-inline")
+                        .html(html);
+                    if (i === item.choices.length - 1) {
+                        item_div
+                            .append("div")
+                            .attr("class", "invalid-feedback")
+                            .html("Please select an answer.");
                     }
+
+                    
                 });
+                choiceContainer.append('hr');
             } else {
                 // open question
-                div.append('textarea')
-                   .attr('class', 'form-control')
-                   .attr('id', `item${item_no}-textarea`)
-                   .attr('rows', 3)
-                   .attr('required', '');
-                div.append('div')
-                   .attr('id', `item${item_no}-feedback`)  
-                   .attr('class', "invalid-feedback")
-                   .html(`Please provide an answer, using at least ${min_text_length} characters.`);
+                div
+                    .append("textarea")
+                    .attr("class", "form-control")
+                    .attr("id", `item${item_no}-textarea`)
+                    .attr("rows", 3)
+                    .attr("required", "");
+                div
+                    .append("div")
+                    .attr("id", `item${item_no}-feedback`)
+                    .attr("class", "invalid-feedback")
+                    .html(
+                        `Please provide an answer, using at least ${min_text_length} characters.`
+                    );
             }
         }
     });
@@ -67,45 +94,45 @@ d3.select(window).on("load", function () {
     data.reverse();
 
     // make the checkboxes valid as soon as one item is selected
-    d3.selectAll('.form-check-input')
-        .on('click', function (d) {
-            // get item_no
-            var item = d3.select(this).attr('id').split('-')[0];
-            d3.selectAll(`.${item}-radios`).classed('is-invalid', false);
-      });
-    // 
+    d3.selectAll(".form-check-input").on("click", function (d) {
+        // get item_no
+        var item = d3.select(this).attr("id").split("-")[0];
+        d3.selectAll(`.${item}-radios`).classed("is-invalid", false);
+    });
+    //
     // make the textarea valid as soon as min_text_length characters are entered
-    d3.selectAll('textarea')
-        .on('keyup', function (d) {
-            var selected = d3.select(this);
-            var text = selected.node().value;
-            if (text.trim().length >= min_text_length) {
-                selected.classed('is-invalid', false);
-            }
-      });
-    // 
+    d3.selectAll("textarea").on("keyup", function (d) {
+        var selected = d3.select(this);
+        var text = selected.node().value;
+        if (text.trim().length >= min_text_length) {
+            selected.classed("is-invalid", false);
+        }
+    });
+    //
 
-    d3.select('#submit-btn').on('click', function () {
+    d3.select("#submit-btn").on("click", function () {
         // calculate task completion time
         var end = performance.now();
         var task_completion_time = end - start;
 
-        console.log('task_completion_time:', task_completion_time);
+        console.log("task_completion_time:", task_completion_time);
 
         // get the form data
         var results = [];
         var errors = false;
-        data.forEach( (item, item_no) => {
-            if ('label' in item) {
-                results.push('');
-            } else if ('question' in item) {
-                if ('choices' in item) {
+        data.forEach((item, item_no) => {
+            if ("label" in item) {
+                results.push("");
+            } else if ("question" in item) {
+                if ("choices" in item) {
                     // multiple choice question
-                    var selected = d3.select(`input[name="item${item_no}-radios"]:checked`);
+                    var selected = d3.select(
+                        `input[name="item${item_no}-radios"]:checked`
+                    );
                     if (selected.node() === null) {
-                        answer = '';
+                        answer = "";
                         mising_data = true;
-                        d3.selectAll(`.item${item_no}-radios`).classed('is-invalid', true);
+                        d3.selectAll(`.item${item_no}-radios`).classed("is-invalid", true);
                         errors = true;
                     } else {
                         var q = 'label[for="' + selected.node().id + '"]';
@@ -113,25 +140,23 @@ d3.select(window).on("load", function () {
                     }
                     results.push({
                         question: item.question,
-                        answer: answer
+                        answer: answer,
                     });
                 } else {
                     // open question
                     var selected = d3.select(`#item${item_no}-textarea`);
-                    var text = selected.node().value
-                    console.log('text:', text);
+                    var text = selected.node().value;
+                    console.log("text:", text);
                     results.push({
                         question: item.question,
-                        answer: text
+                        answer: text,
                     });
-                    if ( text.length < min_text_length ) {
-                        selected.classed('is-invalid', true);
+                    if (text.length < min_text_length) {
+                        selected.classed("is-invalid", true);
                         errors = true;
-                        d3.select(`item${item_no}-feedback`)
-                            .style('display', 'block');
+                        d3.select(`item${item_no}-feedback`).style("display", "block");
                     } else {
-                        d3.select(`.item${item_no}-feedback`)
-                            .style('display', 'hidden');
+                        d3.select(`.item${item_no}-feedback`).style("display", "hidden");
                     }
                 }
             }
@@ -140,7 +165,7 @@ d3.select(window).on("load", function () {
             console.log(results);
         });
 
-        console.log('errors:', errors);
+        console.log("errors:", errors);
 
         event.preventDefault();
         event.stopPropagation();
@@ -151,28 +176,27 @@ d3.select(window).on("load", function () {
             var post_data = {
                 groups: results,
                 task_completion_time: task_completion_time,
-                log: log
+                log: log,
             };
             fetch(post_url, {
-                method: 'POST',
+                method: "POST",
                 body: JSON.stringify(post_data),
-                credentials: 'include',
-                headers: {'Content-Type': 'application/json'}
-            }).then(res => res.json()).then(response => {
-                console.log('POST response:', response);
-                console.log('POST response.headers:', response.headers);
-                // window.location.replace(server_url + "/tasks/?order=" + next_task_order);
-                // window.location = server_url + "/tasks/?order=" + next_task_order;
-                window.location = response.completion_url;
-            }).catch(err => {
-                console.log('POST error:', err);
-            });
+                credentials: "include",
+                headers: { "Content-Type": "application/json" },
+            })
+                .then((res) => res.json())
+                .then((response) => {
+                    console.log("POST response:", response);
+                    console.log("POST response.headers:", response.headers);
+                    // window.location.replace(server_url + "/tasks/?order=" + next_task_order);
+                    // window.location = server_url + "/tasks/?order=" + next_task_order;
+                    window.location = response.completion_url;
+                })
+                .catch((err) => {
+                    console.log("POST error:", err);
+                });
         }
     });
 
     var start = performance.now();
-        
-
 });
-
-
